43fdf802180b5abb6ec321b1ad2e233d37e926b1
NANDINIPASI <145583472+NANDINIPASI@users.noreply.github.com>
2023-12-07 20:04:12 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -2,57 +2,47 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
-const argv = require('minimist')(process.argv.slice(2)); // Use minimist to parse command line arguments
+const argv = require('minimist')(process.argv.slice(2));
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const pathname = parsedUrl.pathname;
 
-  if (pathname === '/home') {
- 
+  if (pathname === '/home.html') {
     const homePath = path.join(__dirname, 'home.html');
-    fs.readFile(homePath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else if (pathname === '/project') {
-    // Serve project.html
+    serveFile(res, homePath);
+  } else if (pathname === '/project.html') {
     const projectPath = path.join(__dirname, 'project.html');
-    fs.readFile(projectPath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else if (pathname === '/registration') {
-    // Serve registration.html
+    serveFile(res, projectPath);
+  } else if (pathname === '/registration.html') {
     const registrationPath = path.join(__dirname, 'registration.html');
-    fs.readFile(registrationPath, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
+    serveFile(res, registrationPath);
+  } else if (pathname === '/favicon.ico') {
+    // Ignore requests for favicon.ico
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   } else {
-    // Handle unknown routes with a 404 response
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
-// Use the supplied port number or default to 3000
-const port = argv.port || 3000;
+function serveFile(res, filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file at path: ${filePath}`);
+      console.error(err);  // Log the detailed error
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+}
+
+const port = argv.port || 5000;
+
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
